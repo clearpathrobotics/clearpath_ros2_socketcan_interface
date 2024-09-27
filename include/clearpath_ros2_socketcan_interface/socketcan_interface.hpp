7d55b4f45437a8bf4c037b1c235e4113ea2e14ff
Luis Camero <lcamero@clearpathrobotics.com>
2024-09-27 17:09:04 -0400

    Initial add of clearpath_ros2_socketcan_interface

---
+++ include/clearpath_ros2_socketcan_interface/socketcan_interface.hpp
@@ -0,0 +1,41 @@
+#ifndef CLEARPATH_ROS2_SOCKETCAN_INTERFACE__SOCKETCAN_INTERFACE_HPP_
+#define CLEARPATH_ROS2_SOCKETCAN_INTERFACE__SOCKETCAN_INTERFACE_HPP_
+
+
+#include <queue>
+#include <mutex>
+
+#include "rclcpp/rclcpp.hpp"
+#include "can_msgs/msg/frame.hpp"
+#include "clearpath_ros2_socketcan_interface/visibility_control.h"
+
+namespace clearpath_ros2_socketcan_interface
+{
+
+class SocketCANInterface
+{
+public:
+  SocketCANInterface(const std::string& canbus, std::shared_ptr<rclcpp::Node> & nh);
+
+  bool recv(can_msgs::msg::Frame::SharedPtr msg);
+  void send(can_msgs::msg::Frame msg);
+
+private:
+  std::string canbus_;  // CANBUS interface
+  std::shared_ptr<rclcpp::Node> nh_;
+
+  std::queue<can_msgs::msg::Frame> can_rx_message_queue_;
+  std::mutex receive_queue_mutex_;
+
+  can_msgs::msg::Frame frame_msg_;
+
+  rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr can_tx_pub_;
+  rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr can_rx_sub_;
+
+  void rxCallback(const can_msgs::msg::Frame::SharedPtr msg);
+};
+
+}
+  // namespace clearpath_ros2_socketcan_interface
+
+#endif  // CLEARPATH_ROS2_SOCKETCAN_INTERFACE__SOCKETCAN_INTERFACE_HPP_
---
+++ src/socketcan_interface.cpp
@@ -0,0 +1,51 @@
+#include "clearpath_ros2_socketcan_interface/socketcan_interface.hpp"
+
+namespace clearpath_ros2_socketcan_interface
+{
+
+SocketCANInterface::SocketCANInterface(const std::string& canbus,
+                                   std::shared_ptr<rclcpp::Node> & nh):
+  canbus_(canbus), nh_(nh)
+{
+  // Rx Subscriber
+  can_rx_sub_ = nh->create_subscription<can_msgs::msg::Frame>(
+    canbus_ + "/rx",
+    rclcpp::SensorDataQoS(),
+    std::bind(&SocketCANInterface::rxCallback, this, std::placeholders::_1));
+
+  // Tx Publisher
+  can_tx_pub_ = nh->create_publisher<can_msgs::msg::Frame>(
+    canbus_ + "/tx",
+    rclcpp::SystemDefaultsQoS());
+}
+
+void SocketCANInterface::rxCallback(const can_msgs::msg::Frame::SharedPtr msg)
+{
+  if (msg)
+  {
+    std::lock_guard<std::mutex> lock(receive_queue_mutex_);
+    can_rx_message_queue_.push(*msg);
+  }
+}
+
+bool SocketCANInterface::recv(can_msgs::msg::Frame::SharedPtr msg)
+{
+  std::lock_guard<std::mutex> lock(receive_queue_mutex_);
+  if (can_rx_message_queue_.empty())
+  {
+    return false;
+  }
+  *msg = can_rx_message_queue_.front();
+  can_rx_message_queue_.pop();
+  return true;
+}
+
+void SocketCANInterface::send(can_msgs::msg::Frame msg)
+{
+  // frame_msg_ = msg.getFrame();
+  // frame_msg_.header.stamp = nh_->get_clock()->now();
+  // frame_msg_.header.frame_id = "base_link";
+  can_tx_pub_->publish(msg);
+}
+
+}  // namespace clearpath_ros2_socketcan_interface
